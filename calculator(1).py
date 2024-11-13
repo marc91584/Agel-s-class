02c448ea667c7ddcce7e38931d498736cf875ca0
marc91584 <marcos_fernandezdepinedo@euneiz.com>
2024-11-13 11:53:51 +0000

    primera version de mi software me queda hacerlo mejor

---
+++ calculator(1).py
@@ -0,0 +1,5 @@
+import calculator
+calculator.add(20,10)
+calculator.multiply(20,10)
+calculator.divide(20,10)
+calculator.subtract(20,10)
---
+++ operaciones.py
@@ -0,0 +1,4 @@
+from calculator import add, multiply
+print(add(20,10))
+print(multiply(20,10))
+
